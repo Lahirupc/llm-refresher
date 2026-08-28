affe9fe41d6adf5eb254feeccfb3fe2b1a9600d5
Lahirupc <lahirupc@yahoo.com>
2026-08-28 10:29:40 +0530

    feat calculator

--- fastapi-expose-llm/main.py
+++ fastapi-expose-llm/main.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
 from logging import getLogger
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain_core.tools import tool
 
 # Load environment variables from .env
 load_dotenv()
@@ -38,24 +39,61 @@ model = ChatGoogleGenerativeAI(
     # other params...
 )
 
+# tools with pydantic models
+# class Add(BaseModel):
+#     """
+#     Add two numbers
+#     """
+#     a: int = Field(description="First number")
+#     b: int = Field(description="Second number")
+
+# class Multiply(BaseModel):
+#     """
+#     Multiply two numbers
+#     """
+#     a: int = Field(description="First number")
+#     b: int = Field(description="Second number")
+
+@tool
+def add(a: int, b: int) -> int:
+    """
+    Add two numbers
+    """
+    return a + b +1
+
+@tool
+def multiply(a: int, b: int) -> int:
+    """
+    Multiply two numbers
+    """
+    return a * b
+
+# bind tools to model
+tools = [add, multiply]
+model = model.bind_tools(tools)
 
 
-
+# request and response models
 class TextRequest(BaseModel):
     text: str
 
 class TextResponse(BaseModel):
-    translated_text: str = Field(
-            description="Translated user sentence"
-        )
-    is_formal: bool = Field(
-        description="Whether the translated text is formal"
-    )
+    translated_text: str = Field(description="Translated user sentence")
+    is_formal: bool = Field(description="Whether the translated text is formal")
 
-structured_model = model.with_structured_output(TextResponse, method="json_mode")
+class CalculatedResponse(BaseModel):
+    result: int = Field(description="The result of the calculation")
+    answer_in_words: str = Field(description="The answer in words")
 
-@app.post("/question", response_model=TextResponse)
-async def handle_text(request: TextRequest):
+
+
+
+@app.get("/")
+async def root():   
+    return {"message": "API is running. Send a POST request to /translate"}
+
+@app.post("/translate", response_model=TextResponse)
+def handle_text(request: TextRequest):
     """
     Accepts text and responds with text
     """
@@ -68,24 +106,54 @@ async def handle_text(request: TextRequest):
     ]
 
     try:
+        # structured output
+        structured_model = model.with_structured_output(TextResponse, method="json_mode")
         output_parser = PydanticOutputParser(pydantic_object=TextResponse)
         format_instructions = output_parser.get_format_instructions()
-        logger.info(f"Format instructions: {format_instructions}")
-
-        ai_msg = await structured_model.ainvoke(f"{messages} {format_instructions}")
+        structured_response = structured_model.invoke(f"{messages} {format_instructions}")
 
         logger.info("Successfully received response from LLM")
-        logger.debug(ai_msg)
+        logger.debug(f"Api response: {structured_response}")
         # enforce the response model
         return TextResponse(
-            translated_text=ai_msg.translated_text,
-            is_formal=ai_msg.is_formal
+            translated_text=structured_response.translated_text,
+            is_formal=structured_response.is_formal
         )
     except Exception as e:
         logger.error(f"Error invoking LLM: {e}", exc_info=True)
         raise HTTPException(status_code=500, detail="Internal server error calling LLM")
 
 
-@app.get("/")
-async def root():
-    return {"message": "API is running. Send a POST request to /question"}
+
+@app.post("/calculate", response_model=CalculatedResponse)
+def handle_text(request: TextRequest):
+    """
+    Accepts text and responds with text
+    """
+    messages = [
+    (
+        "system",
+        "You are a helpful assistant that calculates numbers. Use the tools to answer the user's request.",
+    ),
+    ("human", request.text),
+    ]
+
+    try:
+        # structured output
+        structured_model = model.with_structured_output(CalculatedResponse, method="json_mode")
+        output_parser = PydanticOutputParser(pydantic_object=CalculatedResponse)
+        format_instructions = output_parser.get_format_instructions()
+        structured_response = structured_model.invoke(f"{messages} {format_instructions}")
+        print(f"{messages} {format_instructions}")
+
+        logger.info("Successfully received response from LLM")
+        print(structured_response)
+        logger.debug(f"Api response: {structured_response}")
+        # enforce the response model
+        return CalculatedResponse(
+            result=structured_response.result,
+            answer_in_words=structured_response.answer_in_words
+        )
+    except Exception as e:
+        logger.error(f"Error invoking LLM: {e}", exc_info=True)
+        raise HTTPException(status_code=500, detail="Internal server error calling LLM")
